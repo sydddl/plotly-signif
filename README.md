b61c7aaf7c362ba21fb83a62c101527eafac26ff
DeathSprout <2148519656@qq.com>
2023-09-30 23:10:02 +0800

    updata

--- README.md
+++ README.md
@@ -27,3 +27,27 @@ test.show(save_path="./image/example.png")  # Save usage properties save_path = 
 ```
 
 ![](./image/example.png)
+
+
+## Parameter explanation
+
+- `__init__` : 
+  - **data :** Data frame used for drawing bar chart
+  - **error :** Data frame corresponding to error bars
+  - **text :** Image text label
+- `plot()`
+  - **pic_px :** image size list , [width,height] , The unit is px
+- `add_annotations()`
+  - **annotation_list :** 
+  - **text :** annotation symbol, _default = "***"_ , which can also be 🪙🆙🏅⭐🔥🍋💔😃😭 ... symbols, visible https://www.emojiall.com/zh-hans/copy 
+  - **size :**  symbol font size, _default = 16_
+  - **line_width :** line width, _default = 1.5_
+  - **color :** intergroup annotation color, _default = 'rgba(0,0,0,1)'_
+  - **inne_color :**  annotation color within group, _default = "rgba(100,100,100,1)"_
+  - **y_max_index :** The y-height of the annotation when layer is set to 0, _default = 1.15_ , multiply the height of the tallest bar in the chart by y_max_index to get y_max
+  - **layer_down_index :** control the spacing between upper and lower layers, _default = 0.08_ , layer_down = layer_down_index $ \times $ y_max
+  - **sign_d_index :** distance between line and symbol, _default = 4_ , layer_down / sign_d_index
+  - **line_d_index :** the length of the hanging line, _default = 3.2_ , layer_down / line_d_index
+- `show()`
+  - **save_path :** path to save.
+- `set_color()` : set the color of the column, given a list
--- plotly_signif.py
+++ plotly_signif.py
@@ -67,31 +67,33 @@ class plotly_signif():
                 f.write(scope.transform(self.fig, format="png",scale=scale))
 
     def add_annotations(self,annotation_list=list(),
-                        text="***",size=16,color = 'rgba(0,0,0,1)',inne_color="rgba(20,20,20,0.7)",line_width=2,  # 传到annotation的
-                        y_max_index = 1.15,part_down_index = 0.08,sign_d_index = 4,line_d_index = 3.2,
+                        text="***",size=16,color = 'rgba(0,0,0,1)',inne_color="rgba(100,100,100,1)",line_width=1.5,  # 传到annotation的
+                        y_max_index = 1.15,layer_down_index = 0.08,sign_d_index = 4,line_d_index = 3.2,
                         ):
         '''
         annotation_list: 显著标识的位置, list | tuple,
             (最后一个数值表示y轴位置,0 最高, 最高位置由 y_max 设置, 增大1下落一个part_down的间隔,传进annotationq前会删去) 
             如[0,1,0]表示要画的两个组的x轴次序,[1,0,1,1]表示要画在第2个组内的1、2两个bar间。
-        inne: 是否为组内标识,bool
         text: 标识符号，也可以为 🪙🆙🏅⭐🔥🍋💔😃😭 等符号。可见 https://www.emojiall.com/zh-hans/copy
         size: 标识符号字体大小
         color: 线框颜色; width:线宽度 ; 
         d : 线和标识符号的距离 ; line_d : 线下沿的长度
-        part_down : 上下间距, y_max : 最上层y轴位置
+        layer_down : 上下间距, y_max : 最上层y轴位置
         '''
         bar_max = self.fig.data[0]["y"].max()
         y_max = bar_max * y_max_index
-        part_down = bar_max * part_down_index
-        d = part_down / sign_d_index
-        line_d = part_down / line_d_index
+        layer_down = y_max * layer_down_index
+        d = layer_down / sign_d_index
+        line_d = layer_down / line_d_index
         for i in annotation_list:
             y_layer = i[-1]
-            y = y_max - part_down * y_layer
+            y = y_max - layer_down * y_layer
             self.annotation(anno_at = i[:-1],y_max = y,text=text,size=size,color =color,inne_color=inne_color,line_width=line_width,d = d,line_d = line_d)        
 
     def annotation(self,anno_at,inne=False,**kwargs):
+        '''
+        inne: 是否为组内标识,bool
+        '''
         assert isinstance(anno_at, list or tuple), "标识指示为空或格式错误, list | tuple"
         d = kwargs["d"]
         line_d = kwargs["line_d"]
@@ -110,22 +112,23 @@ class plotly_signif():
                 diff1 = (anno_at[1] - mean) * between
                 diff2 = (anno_at[2] - mean) * between
             self.fig.add_annotation(text=kwargs["text"],name="p-value",xref="x", yref="y",
-                x=group_at + (diff1+diff2)/2 , y=y_max, showarrow=False,font=dict(size=kwargs["size"], color=kwargs["inne_color"]),)
-            self.fig.add_shape(type="line",x0=group_at + diff1,y0=y_max-d, x1=group_at + diff2, y1=y_max-d,
+                x=group_at + (diff1+diff2)/2 , y=y_max + d, showarrow=False,font=dict(size=kwargs["size"], color=kwargs["inne_color"]),)
+            self.fig.add_shape(type="line",x0=group_at + diff1,y0=y_max, x1=group_at + diff2, y1=y_max,
                                line=dict(color=kwargs["inne_color"],width=kwargs["line_width"]))
-            self.fig.add_shape(type="line",x0=group_at + diff1,y0=y_max-d-line_d, x1=group_at + diff1, y1=y_max-d,
+            self.fig.add_shape(type="line",x0=group_at + diff1,y0=y_max-line_d, x1=group_at + diff1, y1=y_max,
                                line=dict(color=kwargs["inne_color"],width=kwargs["line_width"]))
-            self.fig.add_shape(type="line",x0=group_at + diff2,y0=y_max-d-line_d, x1=group_at + diff2, y1=y_max-d,
+            self.fig.add_shape(type="line",x0=group_at + diff2,y0=y_max-line_d, x1=group_at + diff2, y1=y_max,
                                line=dict(color=kwargs["inne_color"],width=kwargs["line_width"]))
+
         else:
             x_local = np.array(anno_at).mean()
             self.fig.add_annotation(text=kwargs["text"],name="p-value",xref="x", yref="y",
-                        x=x_local, y=y_max, showarrow=False,font=dict(size=kwargs["size"], color="black"),)
-            self.fig.add_shape(type="line",x0=self.Group_name[anno_at[0]],y0=y_max-d, x1=self.Group_name[anno_at[1]], y1=y_max-d,
+                        x=x_local, y=y_max+d, showarrow=False,font=dict(size=kwargs["size"], color="black"),)
+            self.fig.add_shape(type="line",x0=self.Group_name[anno_at[0]],y0=y_max, x1=self.Group_name[anno_at[1]], y1=y_max,
                                line=dict(color=kwargs["color"],width=kwargs["line_width"]))
-            self.fig.add_shape(type="line",x0=self.Group_name[anno_at[0]],y0=y_max-d-line_d, x1=self.Group_name[anno_at[0]], y1=y_max-d,
+            self.fig.add_shape(type="line",x0=self.Group_name[anno_at[0]],y0=y_max-line_d, x1=self.Group_name[anno_at[0]], y1=y_max,
                                line=dict(color=kwargs["color"],width=kwargs["line_width"]))
-            self.fig.add_shape(type="line",x0=self.Group_name[anno_at[1]],y0=y_max-d-line_d, x1=self.Group_name[anno_at[1]], y1=y_max-d,
+            self.fig.add_shape(type="line",x0=self.Group_name[anno_at[1]],y0=y_max-line_d, x1=self.Group_name[anno_at[1]], y1=y_max,
                                line=dict(color=kwargs["color"],width=kwargs["line_width"]))
 
     def set_color(self,color):
