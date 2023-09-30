76a8e684871f7266843d67e5a5d5228410d88b5c
DeathSprout <2148519656@qq.com>
2023-09-30 21:45:54 +0800

    test

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # plotly-signif
  A python example of drawing a box plot based on the plotly package, with freely definable statistical annotations
+
+
+## How to use
+
+```py
+from plotly_signif import *
+import pandas as pd
+iris = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+df1 = iris.groupby("species").mean()
+df2 = iris.groupby("species").sem()
+```
+
+```py
+test = plotly_signif(data=df1,error=df2,text={"x":"","y":""})  
+test.plot()
+# Add statistical annotation
+annotation_list = [[0,1,1],[2,0,3,-1],[2,1,2,2],[0,1,3,6],[0,2,3,10]]
+test.add_annotations(annotation_list = annotation_list,text="***")   
+test.add_annotations(annotation_list = [[1,2,3,4]],text="🏅",sign_d_index=2) 
+test.add_annotations(annotation_list = [[1,1,2,5]],text="😭",sign_d_index=2) 
+test.add_annotations(annotation_list = [[1,0,2,2]],text="⭐⭐⭐",sign_d_index=2,size=12) 
+test.add_annotations(annotation_list = [[1,2,0]],text="ns",size=14,sign_d_index=2) 
+# draw/save
+test.show(save_path="./image/example.png")  # Save usage properties save_path = "./image/example.png" 
+```
+
+![](./image/example.png)
--- plotly_signif.py
+++ plotly_signif.py
@@ -3,7 +3,7 @@ import numpy as np
 import plotly.graph_objects as go
 from kaleido.scopes.plotly import PlotlyScope
 
-class Plot_Box_with_annotation():
+class plotly_signif():
     def __init__(self,data=pd.DataFrame,error=pd.DataFrame,text={},json={}) -> None:
         self.fig = go.Figure()
         self.df = data
@@ -13,9 +13,9 @@ class Plot_Box_with_annotation():
         self.inne_num = self.df.shape[1] #一组几个柱
         self.width = 0.5 / self.inne_num  # 柱宽
         self.bargap = 0.25 # 表示类内间隔
-        self.color = ["#5044f3",'#8ca0f7',"rgba(104, 135, 255,0.4)",'#6DECB9', "rgba(154, 225, 155,0.4)"]  #默认颜色
+        self.color = ["#5044f3",'#8ca0f7',"rgba(104, 135, 255,0.4)",'#4DECB9', "rgba(154, 225, 155,0.4)"]  #默认颜色
 
-    def plot(self,pic_px=[500,550],save_path=None,scale=400/96):
+    def plot(self,pic_px=[500,550]):
         for i, column in enumerate(self.df.columns):
             self.fig.add_trace(go.Bar(
                 x=self.df.index,
@@ -37,7 +37,7 @@ class Plot_Box_with_annotation():
         self.fig.update_xaxes(title_text=self.text["x"], showline=True, linewidth=3, linecolor='black', tickangle=0,gridcolor='#EEEEEE',
                     title_font=dict(size=16, color='black'),ticks='outside',
                     tickfont=dict(size=18, color='black'))
-        self.fig.update_yaxes(title_text=self.text["y"],range=[0, 1],showline=True, linewidth=3, linecolor='black', tickfont=dict(size=16),
+        self.fig.update_yaxes(title_text=self.text["y"],showline=True, linewidth=3, linecolor='black', tickfont=dict(size=16),
                  title_standoff=10,tickmode='auto',showticklabels=True,ticks='outside',
                  title_font=dict(size=20, color='black'))
         self.fig.update_layout(
@@ -56,6 +56,8 @@ class Plot_Box_with_annotation():
                 width=pic_px[0],height=pic_px[1], # 图 -> 宽/高
             )
         self.fig.update_traces(textfont_family="Raleway", selector=dict(type='bar'))
+
+    def show(self,save_path=None,scale=400/96):
         self.fig.show()
         if save_path is None:
             pass
@@ -65,8 +67,9 @@ class Plot_Box_with_annotation():
                 f.write(scope.transform(self.fig, format="png",scale=scale))
 
     def add_annotations(self,annotation_list=list(),
-                        text="***",size=17,color = 'rgba(0,0,0,1)',inne_color="rgba(0,0,0,0.5)",line_width=2,d = 0.05,line_d = 0.02,  # 传到annotation的
-                        part_down = 0.08,y_max = 1.02):
+                        text="***",size=16,color = 'rgba(0,0,0,1)',inne_color="rgba(20,20,20,0.7)",line_width=2,  # 传到annotation的
+                        y_max_index = 1.15,part_down_index = 0.08,sign_d_index = 4,line_d_index = 3.2,
+                        ):
         '''
         annotation_list: 显著标识的位置, list | tuple,
             (最后一个数值表示y轴位置,0 最高, 最高位置由 y_max 设置, 增大1下落一个part_down的间隔,传进annotationq前会删去) 
@@ -78,6 +81,11 @@ class Plot_Box_with_annotation():
         d : 线和标识符号的距离 ; line_d : 线下沿的长度
         part_down : 上下间距, y_max : 最上层y轴位置
         '''
+        bar_max = self.fig.data[0]["y"].max()
+        y_max = bar_max * y_max_index
+        part_down = bar_max * part_down_index
+        d = part_down / sign_d_index
+        line_d = part_down / line_d_index
         for i in annotation_list:
             y_layer = i[-1]
             y = y_max - part_down * y_layer
@@ -101,8 +109,8 @@ class Plot_Box_with_annotation():
             else:
                 diff1 = (anno_at[1] - mean) * between
                 diff2 = (anno_at[2] - mean) * between
-            self.fig.add_annotation(text=kwargs["text"],name="p-value",xref="x", yref="paper",
-                x=group_at + (diff1+diff2)/2 , y=y_max, showarrow=False,font=dict(size=kwargs["size"] - 3, color=kwargs["inne_color"]),)
+            self.fig.add_annotation(text=kwargs["text"],name="p-value",xref="x", yref="y",
+                x=group_at + (diff1+diff2)/2 , y=y_max, showarrow=False,font=dict(size=kwargs["size"], color=kwargs["inne_color"]),)
             self.fig.add_shape(type="line",x0=group_at + diff1,y0=y_max-d, x1=group_at + diff2, y1=y_max-d,
                                line=dict(color=kwargs["inne_color"],width=kwargs["line_width"]))
             self.fig.add_shape(type="line",x0=group_at + diff1,y0=y_max-d-line_d, x1=group_at + diff1, y1=y_max-d,
@@ -111,7 +119,7 @@ class Plot_Box_with_annotation():
                                line=dict(color=kwargs["inne_color"],width=kwargs["line_width"]))
         else:
             x_local = np.array(anno_at).mean()
-            self.fig.add_annotation(text=kwargs["text"],name="p-value",xref="x", yref="paper",
+            self.fig.add_annotation(text=kwargs["text"],name="p-value",xref="x", yref="y",
                         x=x_local, y=y_max, showarrow=False,font=dict(size=kwargs["size"], color="black"),)
             self.fig.add_shape(type="line",x0=self.Group_name[anno_at[0]],y0=y_max-d, x1=self.Group_name[anno_at[1]], y1=y_max-d,
                                line=dict(color=kwargs["color"],width=kwargs["line_width"]))
